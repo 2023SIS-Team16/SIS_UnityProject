4147fd0ea8335b62a1c821967046876a40c987e9
Lachlan Garrity <lachlangarrity@gmail.com>
2023-09-10 12:08:46 +1000

    ScriptableObjects

---
+++ SIS_Hololens/Assets/Scripts/SignScriptableObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "SignScriptableObject", menuName = "ScriptableObjects/Sign")]
+public class SignScriptableObject : ScriptableObject
+{
+    public char character;
+    public Sprite signSprite;
+}
