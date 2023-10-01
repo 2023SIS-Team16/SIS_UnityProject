5b277a3e07de001e3976346d3974cdb2a1b964c4
Lachlan Garrity <lachlangarrity@gmail.com>
2023-10-01 21:03:16 +1100

    SaveWebcam

--- SIS_Hololens/Assets/Scripts/GetWebcam.cs
+++ SIS_Hololens/Assets/Scripts/GetWebcam.cs
@@ -7,6 +7,8 @@ using UnityEngine.Windows.WebCam;
 public class GetWebcam : MonoBehaviour
 {
     private WebCamTexture _webCamTexture;
+    [SerializeField] private Texture2D frame;
+    private int _captureCount = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,28 @@ public class GetWebcam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_webCamTexture.didUpdateThisFrame)
+        {
+            frame = new Texture2D(_webCamTexture.width, _webCamTexture.height);
+            frame.SetPixels(_webCamTexture.GetPixels());
+            frame.Apply();
+            
+            string path = @"F:/temp/UnityTest/";
+            
+            string savePath = path;
+            System.IO.File.WriteAllBytes(savePath + _captureCount.ToString() + ".png", frame.EncodeToPNG());
+            _captureCount++;
+            Debug.Log(_captureCount);
+        }
+    }
+
+    public Texture2D GetCurrentFrame()
+    {
+        if (frame)
+        {
+            return frame;
+        }
+
+        return new Texture2D(_webCamTexture.width, _webCamTexture.height);
     }
 }
